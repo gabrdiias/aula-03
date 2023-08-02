909d50af4c71382de31ba50b83c74f32c575d988
gabrdiias <127976037+gabrdiias@users.noreply.github.com>
2023-08-02 11:51:23 -0300

    Add files via upload

---
+++ ativ03.js
@@ -0,0 +1,40 @@
+//Questão 01
+
+//armazena a minha idade em anos humanos.
+let myAge = 16
+
+//Questão 02
+
+//esses são os dois primeiros anos da vida de um cão, que contam mais do que os anos subsequentes.
+let earlyYears = 2
+
+//Questão 03
+
+earlyYears *=10.5
+
+//Questão 04
+
+let laterYears = myAge - 2
+//laterYears são os meus anos, após a subtração dos 2 anos que foram multiplicados em 'earlyYears'
+
+//Questão 05
+
+laterYears *4
+console.log('Esta é a minha idade em anos de cachorro'+ ': ' + laterYears *4)
+
+//Questão 06
+
+console.log(earlyYears); 
+console.log(laterYears); 
+
+// Questão 07//
+
+let myAgeInDogYears = earlyYears + laterYears; // Representa minha idade em anos de cão.
+
+// Questão 08//
+
+let myName = "Gabriel Dias".toLowerCase(); // Meu nome em letras minúsculas.
+
+// Questão 09//
+
+console.log(`Meu nome é ${myName}. Tenho ${myAge} anos em anos humanos, que são ${myAgeInDogYears} anos em anos de cão.`);
